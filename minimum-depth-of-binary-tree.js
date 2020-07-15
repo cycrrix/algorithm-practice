2d75f7302dcd554b517a7bd35c2be6fbbd63b18d
gscaiyucheng <gscaiyucheng@163.com>
2020-07-15 22:40:41 +0800

    add balanced tree

--- minimum-depth-of-binary-tree.js
+++ minimum-depth-of-binary-tree.js
@@ -9,8 +9,17 @@
  * @param {TreeNode} root
  * @return {number}
  */
-var minDepth = function(root) {
-
+var minDepth = function (root) {
+  if (root === null) {
+    return 0;
+  }
+  if (root.left === null) {
+    return 1 + minDepth(root.right);
+  }
+  if (root.right === null) {
+    return 1 + minDepth(root.left);
+  }
+  return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
 };
 
 /**
@@ -18,17 +27,17 @@ var minDepth = function(root) {
  * 给定一个二叉树，找出其最小深度。
  * 最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
  * 说明: 叶子节点是指没有子节点的节点。
- * 
+ *
  * 示例:
  * 给定二叉树 [3,9,20,null,null,15,7],
- * 
+ *
  *     3
  *    / \
  *   9  20
  *     /  \
  *    15   7
  * 返回它的最小深度  2.
- * 
+ *
  * 思路
  * https://leetcode-cn.com/problems/er-cha-shu-de-shen-du-lcof/solution/mian-shi-ti-55-i-er-cha-shu-de-shen-du-xian-xu-bia/
  */
