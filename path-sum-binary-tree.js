a95a9fbd714031305d4e8de3ffa4802b0e780a91
gscaiyucheng <gscaiyucheng@163.com>
2020-07-16 17:04:24 +0800

    add binary-tree

---
+++ path-sum-binary-tree.js
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} sum
+ * @return {number[][]}
+ */
+var pathSum = function (root, sum) {
+  const result = [];
+  const path = [];
+  recur(root, sum);
+  return result;
+
+  function recur(root, tar) {
+    if (root === null) {
+      return;
+    }
+    path.push(root.val);
+    tar -= root.val;
+    if (tar === 0 && root.left === null && root.right === null) {
+      result.push([...path]);
+    }
+    recur(root.left, tar);
+    recur(root.right, tar);
+    path.pop();
+  }
+};
+
+/**
+ * 剑指 Offer 34. 二叉树中和为某一值的路径
+ * 输入一棵二叉树和一个整数，打印出二叉树中节点值的和为输入整数的所有路径。从树的根节点开始往下一直到叶节点所经过的节点形成一条路径。
+ * 示例:
+ * 给定如下二叉树，以及目标和 sum = 22，
+ * 
+ *               5
+ *              / \
+ *             4   8
+ *            /   / \
+ *           11  13  4
+ *          /  \    / \
+ *         7    2  5   1
+ * 返回:
+ * [
+ *    [5,4,11,2],
+ *    [5,8,4,5]
+ * ]
+ * 思路
+ * https://leetcode-cn.com/problems/er-cha-shu-zhong-he-wei-mou-yi-zhi-de-lu-jing-lcof/solution/mian-shi-ti-34-er-cha-shu-zhong-he-wei-mou-yi-zh-5/
+ */
