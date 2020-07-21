8825b90027700a70205a70209c1c4c162239d7ea
gscaiyucheng <gscaiyucheng@163.com>
2020-07-21 23:55:40 +0800

    modify tree

--- serialize-and-deserialize-binary-tree.js
+++ serialize-and-deserialize-binary-tree.js
@@ -12,27 +12,9 @@
  * @param {TreeNode} root
  * @return {string}
  */
-var serialize = function (pRoot, arr = []) {
-  if (!pRoot) {
-    arr.push("#");
-  } else {
-    arr.push(pRoot.val);
-    serialize(pRoot.left, arr);
-    serialize(pRoot.right, arr);
-  }
-  return arr.join(",");
+var serialize = function(root) {
+    
 };
-console.log(
-  serialize({
-    val: 1,
-    left: { val: 2, left: null, right: null },
-    right: {
-      val: 3,
-      left: { val: 4, right: null, left: null },
-      right: { val: 5, right: null, left: null },
-    },
-  })
-);
 
 /**
  * Decodes your encoded data to tree.
@@ -40,7 +22,9 @@ console.log(
  * @param {string} data
  * @return {TreeNode}
  */
-var deserialize = function (data) {};
+var deserialize = function(data) {
+    
+};
 
 /**
  * Your functions will be called as such:
