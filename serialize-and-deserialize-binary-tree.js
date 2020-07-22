ffd424201ff25858538bc701e2e6ee023083f50f
gscaiyucheng <gscaiyucheng@163.com>
2020-07-23 00:21:38 +0800

    modify tree

--- serialize-and-deserialize-binary-tree.js
+++ serialize-and-deserialize-binary-tree.js
@@ -23,8 +23,8 @@ var serialize = function (root) {
       return;
     }
     res.push(root.val);
-    buildString(root.left,res);
-    buildString(root.right,res);
+    buildString(root.left, res);
+    buildString(root.right, res);
   }
 };
 
@@ -37,17 +37,17 @@ var serialize = function (root) {
  */
 var deserialize = function (data) {
   if (!data) {
-    return null
+    return null;
   }
-  return buildTree(data.split(','))
-  function buildTree(list){
-    const curr=list.shift();
-    if (curr==='X') {
+  return buildTree(data.split(","));
+  function buildTree(list) {
+    const curr = list.shift();
+    if (curr === "X") {
       return null;
     }
-    const node = new TreeNode(curr)
-    node.left=buildTree(list)
-    node.right=buildTree(list)
+    const node = new TreeNode(curr);
+    node.left = buildTree(list);
+    node.right = buildTree(list);
     return node;
   }
 };
@@ -57,28 +57,28 @@ var deserialize = function (data) {
  * deserialize(serialize(root));
  */
 
- /**
+/**
  * 297. 二叉树的序列化与反序列化
  * 序列化是将一个数据结构或者对象转换为连续的比特位的操作，进而可以将转换后的数据存储在一个文件或者内存中，同时也可以通过网络传输到另一个计算机环境，采取相反方式重构得到原数据。
- * 
+ *
  * 请设计一个算法来实现二叉树的序列化与反序列化。这里不限定你的序列 / 反序列化算法执行逻辑，你只需要保证一个二叉树可以被序列化为一个字符串并且将这个字符串反序列化为原始的树结构。
- * 
- * 示例: 
- * 
+ *
+ * 示例:
+ *
  * 你可以将以下二叉树：
- * 
+ *
  *     1
  *    / \
  *   2   3
  *      / \
  *     4   5
- * 
+ *
  * 序列化为 "[1,2,3,null,null,4,5]"
  * 提示: 这与 LeetCode 目前使用的方式一致，详情请参阅 LeetCode 序列化二叉树的格式。你并非必须采取这种方式，你也可以采用其他的方法解决这个问题。
- * 
+ *
  * 说明: 不要使用类的成员 / 全局 / 静态变量来存储状态，你的序列化和反序列化算法应该是无状态的。
  * 思路：
- * 
+ *
  * 题解：
  * http://www.conardli.top/docs/dataStructure/%E4%BA%8C%E5%8F%89%E6%A0%91/%E5%BA%8F%E5%88%97%E5%8C%96%E4%BA%8C%E5%8F%89%E6%A0%91.html
  * https://leetcode-cn.com/problems/serialize-and-deserialize-binary-tree/solution/shou-hui-tu-jie-gei-chu-dfshe-bfsliang-chong-jie-f/
