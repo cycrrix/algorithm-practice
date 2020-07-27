6ff3239e80c45fcbb82f4cec7eb9b5fea0b38e0a
gscaiyucheng <gscaiyucheng@163.com>
2020-07-28 07:58:39 +0800

    modify

---
+++ reverse-linked-list.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * 双指针迭代
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+  let pre =null;
+  let curr = head;
+  let temp =null;
+  while(curr!==null){
+    temp = curr.next;
+    curr.next=pre;
+    pre= curr;
+    curr = temp;
+  }
+  return pre;
+};
+
+/**
+ * 递归
+ * @param {ListNode} curr
+ * @return {ListNode}
+ */
+var reverseList = function(curr) {
+  if (curr ===null|| curr.next===null) {
+    return curr;
+  }
+  const 
+};
