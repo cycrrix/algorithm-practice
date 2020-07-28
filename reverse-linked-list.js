a9966a2987511406de527078b8c7da841a58ef6b
gscaiyucheng <gscaiyucheng@163.com>
2020-07-29 00:16:06 +0800

    modify

--- reverse-linked-list.js
+++ reverse-linked-list.js
@@ -37,3 +37,17 @@ var reverseList = function (curr) {
   curr.next = null;
   return tail;
 };
+
+/**
+ * 剑指 Offer 24. 反转链表
+ * 定义一个函数，输入一个链表的头节点，反转该链表并输出反转后链表的头节点。
+ * 
+ *  
+ * 
+ * 示例:
+ * 
+ * 输入: 1->2->3->4->5->NULL
+ * 输出: 5->4->3->2->1->NULL
+ * 迭代思路
+ * https://leetcode-cn.com/problems/reverse-linked-list/solution/dong-hua-yan-shi-206-fan-zhuan-lian-biao-by-user74/
+ */
