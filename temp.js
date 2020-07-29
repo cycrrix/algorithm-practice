6f07702a424e3acab0fc1a7cc5f9cfe2191ca0f3
gscaiyucheng <gscaiyucheng@163.com>
2020-07-29 23:56:56 +0800

    add linked list

--- temp.js
+++ temp.js
@@ -1,31 +1,20 @@
-var copyRandomList = function (head) {
-  if (head === null) {
-    return null
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} curr
+ * @return {ListNode}
+ */
+var reverseList = function(curr) {
+  if (curr===null||curr.next===null) {
+    return curr
   }
-  let curr = head;
-  while (curr!==null) {
-    const newNode = new Node(curr.val);
-    newNode.next=curr.next;
-    curr.next = newNode;
-    curr=newNode.next;
-  }
-  curr =head;
-  while (curr!==null) {
-    if (curr.random!==null) {
-      curr.next.random=curr.random.next;
-    }
-    curr=curr.next.next;
-  }
-  curr=head;
-  const copyHead = new Node(0);
-  let copyCurr=copyHead;
-  while (curr!==null) {
-    copyCurr.next = curr.next;
-    copyCurr = copyCurr.next;
-    
-    curr.next = curr.next.next;
-    
-    curr = curr.next;
-  }
-  return copyHead.next;
+  const tail = reverseList(curr.next);
+  curr.next.next = curr;
+  curr.next=null;
+  return tail;
 };
