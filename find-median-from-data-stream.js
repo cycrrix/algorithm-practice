42678cb6d8a07998f3cd5d98f247853f51696e91
gscaiyucheng <gscaiyucheng@163.com>
2020-08-20 10:00:59 +0800

    add

--- find-median-from-data-stream.js
+++ find-median-from-data-stream.js
@@ -1,68 +1,121 @@
-// ac地址：https://leetcode-cn.com/problems/find-median-from-data-stream/
-// 原文地址：https://xxoo521.com/2020-02-27-find-median-from-data-stream/
-
-const defaultCmp = (x, y) => x > y; // 默认是最大堆
+const defaultCmp = (x, y) => x < y; // 默认是小顶堆
 
 const swap = (arr, i, j) => ([arr[i], arr[j]] = [arr[j], arr[i]]);
 
-class PriorityQuene {
-    /**
-     * 默认是最大堆
-     * @param {Function} cmp
-     */
-    constructor(cmp = defaultCmp) {
-        this.container = [];
-        this.cmp = cmp;
+class Heap {
+  /**
+   * 默认是最大堆
+   * @param {Function} cmp
+   */
+  constructor(cmp = defaultCmp) {
+    this.container = [];
+    this.cmp = cmp;
+  }
+
+  // 插入
+  insert(data) {
+    const { container, cmp } = this;
+
+    container.push(data);
+    let index = container.length - 1;
+    while (index) {
+      let parent = Math.floor((index - 1) / 2);
+      if (!cmp(container[index], container[parent])) {
+        return;
+      }
+      swap(container, index, parent);
+      index = parent;
+    }
+  }
+
+  // 删除
+  delete() {
+    const { container, cmp } = this;
+    if (!container.length) {
+      return null;
     }
 
-    // 入队
-    insert(data) {
-        const { container, cmp } = this;
+    swap(container, 0, container.length - 1);
+    const res = container.pop();
+    const length = container.length;
+    let index = 0,
+      exchange = index * 2 + 1;
 
-        container.push(data);
-        let index = container.length - 1;
-        while (index) {
-            let parent = Math.floor((index - 1) / 2);
-            if (!cmp(container[index], container[parent])) {
-                return;
-            }
-            swap(container, index, parent);
-            index = parent;
-        }
+    while (exchange < length) {
+      // // 以最大堆的情况来说：如果有右节点，并且右节点的值大于左节点的值
+      let right = index * 2 + 2;
+      if (right < length && cmp(container[right], container[exchange])) {
+        exchange = right;
+      }
+      if (!cmp(container[exchange], container[index])) {
+        break;
+      }
+      swap(container, exchange, index);
+      index = exchange;
+      exchange = index * 2 + 1;
     }
 
-    // 出队
-    extract() {
-        const { container, cmp } = this;
-        if (!container.length) {
-            return null;
-        }
-
-        swap(container, 0, container.length - 1);
-        const res = container.pop();
-        const length = container.length;
-        let index = 0,
-            exchange = index * 2 + 1;
-
-        while (exchange < length) {
-            // // 以最大堆的情况来说：如果有右节点，并且右节点的值大于左节点的值
-            let right = index * 2 + 2;
-            if (right < length && cmp(container[right], container[exchange])) {
-                exchange = right;
-            }
-            if (!cmp(container[exchange], container[index])) {
-                break;
-            }
-            swap(container, exchange, index);
-            index = exchange;
-            exchange = index * 2 + 1;
-        }
-
-        return res;
-    }
-
-    top() {
-        if (this.container.length) return this.container[0];
-        return null;
-    }
+    return res;
+  }
 }
+
+/**
+ * initialize your data structure here.
+ */
+var MedianFinder = function () {
+  this.A = new Heap(); // 默认为小顶堆，保存较大的一半
+  this.B = new Heap((x, y) => x > y); // 大顶堆，保存较小的一半
+};
+
+/**
+ * @param {number} num
+ * @return {void}
+ */
+MedianFinder.prototype.addNum = function (num) {
+  if (this.A.container.length !== this.B.container.length) {
+    this.A.insert(num);
+    this.B.insert(this.A.delete());
+  } else {
+    this.B.insert(num);
+    this.A.insert(this.B.delete());
+  }
+};
+
+/**
+ * @return {number}
+ */
+MedianFinder.prototype.findMedian = function () {
+  return this.A.container.length !== this.B.container.length
+    ? this.A.container[0]
+    : (this.A.container[0] + this.B.container[0]) / 2;
+};
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * var obj = new MedianFinder()
+ * obj.addNum(num)
+ * var param_2 = obj.findMedian()
+ */
+
+/**
+ * 剑指 Offer 41. 数据流中的中位数
+ * 中位数是有序列表中间的数。如果列表长度是偶数，中位数则是中间两个数的平均值。
+ * 例如，
+ * [2,3,4] 的中位数是 3
+ * [2,3] 的中位数是 (2 + 3) / 2 = 2.5
+ * 设计一个支持以下两种操作的数据结构：
+ * void addNum(int num) - 从数据流中添加一个整数到数据结构中。
+ * double findMedian() - 返回目前所有元素的中位数。
+ *
+ * 示例：
+ * addNum(1)
+ * addNum(2)
+ * findMedian() -> 1.5
+ * addNum(3)
+ * findMedian() -> 2
+ *
+ * 题解：
+ * https://leetcode-cn.com/problems/shu-ju-liu-zhong-de-zhong-wei-shu-lcof/solution/mian-shi-ti-41-shu-ju-liu-zhong-de-zhong-wei-shu-y/
+ * javascript二叉堆实现：
+ * https://leetcode-cn.com/problems/shu-ju-liu-zhong-de-zhong-wei-shu-lcof/solution/tu-xie-zheng-li-bao-li-fa-er-fen-cha-zhao-shou-don/
+ */
--- temp.js
+++ temp.js
@@ -1,43 +1,85 @@
-//利用两次二分法分别确定target的左右边界（左右边界为target值序列的左/右一位，因此最终结果是right-left-1）
-var search = function (nums, target) {
-  if (nums.length === 0) {
-    return 0;
-  }
-  //初始左右指针位置
-  let i = 0;
-  let j = nums.length - 1;
-  //第一次二分：找right边界
-  //这边是“小于等于”，因此当循环结束后，ij不重合，且如果存在target值的话，i的位置就是右边界（target值序列右边第一个大于target值的位置
-  while (i <= j) {
-    let mid = (i + j) >> 1;
-    //这里包含“等于”，目的是为了确定右边界，就是说当mid等于target时，因为不确定后面还有没有target，在闭区间[m+1,j]中找右边界right
-    if (nums[mid] <= target) {
-      i = mid + 1;
-    } else {
-      j = mid - 1;
-    }
-  }
-  //查找完右边界 right=i 后，则 nums[j] 指向最右边的 target （若存在）
-  //在更新right边界值之前，需要判断这个数组中是否存在target，如果不存在（看j指向的位置是不是target）
-  if (j >= 0 && nums[j] !== target) {
-    return 0;
-  }
-  let right = i; //更新right边界
-  //重置指针
-  i = 0;
-  j = nums.length - 1;
-  //第二次二分：找left边界
-  //结束之后，j指向target序列左边第一个小于它的位置，i指向target（经过上面判断，target一定存在）
-  while (i <= j) {
-    let mid = (i + j) >> 1;
-    //这里是包含"等于”，目的是为了确定左边界，就是说当mid等于target时，因为不确定左边还有没有target，在闭区间[i,m-1]中找左边界left
-    if (nums[mid] >= target) {
-      j = mid - 1;
-    } else {
-      i = mid + 1;
-    }
-  }
-  //更新左指针
-  let left = j;
-  return right - left - 1;
+const defaultCmp = (x, y) => x < y;
+const swap = (array, i, j) => {
+  [array[i], array[j]] = [array[j], array[i]];
 };
+class Heap {
+  constructor(cmp = defaultCmp) {
+    this.container = [];
+    this.cmp = cmp;
+  }
+  insert(num) {
+    const { container, cmp } = this;
+    container.push(num);
+    let index = container.length - 1;
+    while (index) {
+      let parentIndex = Math.floor((index - 1) / 2);
+      if (!cmp(container[index], container[parentIndex])) {
+        break;
+      }
+      cmp(container, index, parentIndex);
+      index = parentIndex;
+    }
+  }
+  delete() {
+    const { container, cmp } = this;
+    if (container.length === 0) {
+      return null;
+    }
+    swap(container, 0, container.length - 1);
+    const result = container.pop();
+    let index = 0,
+      exchange = index * 2 + 1;
+    while (exchange < length) {
+      let right = index * 2 + 2;
+      if (right < length && cmp(container[right], container[exchange])) {
+        exchange = right;
+      }
+      if (!cmp(container[exchange], container[index])) {
+        break;
+      }
+      swap(container, index, exchange);
+      index = exchange;
+    }
+    return result;
+  }
+}
+
+/**
+ * initialize your data structure here.
+ */
+var MedianFinder = function () {
+  this.A = new Heap();
+  this.B = new Heap((x, y) => x - y);
+};
+
+/**
+ * @param {number} num
+ * @return {void}
+ */
+MedianFinder.prototype.addNum = function (num) {
+  const { A, B } = this;
+  if (A.container.length !== B.container.length) {
+    A.insert(num);
+    B.insert(A.delete());
+  } else {
+    B.insert(num);
+    A.insert(B.delete());
+  }
+};
+
+/**
+ * @return {number}
+ */
+MedianFinder.prototype.findMedian = function () {
+  const { A, B } = this;
+  return A.container.length !== B.container.length
+    ? A.container[0]
+    : (A.container[0] + B.container[0]) / 2;
+};
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * var obj = new MedianFinder()
+ * obj.addNum(num)
+ * var param_2 = obj.findMedian()
+ */
