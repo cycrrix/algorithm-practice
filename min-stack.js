235ae2ae62483faa8906a240beb8d29885082ce2
gscaiyucheng <gscaiyucheng@163.com>
2020-08-27 22:59:11 +0800

    add stack

--- min-stack.js
+++ min-stack.js
@@ -2,8 +2,8 @@
  * initialize your data structure here.
  */
 var MinStack = function () {
-  this.A = [];
-  this.B = [];
+  this.A = []; // 数据栈
+  this.B = []; // 辅助栈
 };
 
 /**
@@ -49,4 +49,22 @@ MinStack.prototype.min = function () {
  * obj.pop()
  * var param_3 = obj.top()
  * var param_4 = obj.min()
+ */
+
+/**
+ * 剑指 Offer 30. 包含min函数的栈
+ * 定义栈的数据结构，请在该类型中实现一个能够得到栈的最小元素的 min 函数在该栈中，调用 min、push 及 pop 的时间复杂度都是 O(1)。
+ *
+ * 示例:
+ * MinStack minStack = new MinStack();
+ * minStack.push(-2);
+ * minStack.push(0);
+ * minStack.push(-3);
+ * minStack.min();   --> 返回 -3.
+ * minStack.pop();
+ * minStack.top();      --> 返回 0.
+ * minStack.min();   --> 返回 -2.
+ *
+ * 题解：
+ * https://leetcode-cn.com/problems/bao-han-minhan-shu-de-zhan-lcof/solution/mian-shi-ti-30-bao-han-minhan-shu-de-zhan-fu-zhu-z/
  */
