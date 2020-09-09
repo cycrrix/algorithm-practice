42be3a75aa441d8f2036a4906fcd56f17cf22aa4
gscaiyucheng <gscaiyucheng@163.com>
2020-09-09 20:16:39 +0800

    add heap

--- temp.js
+++ temp.js
@@ -1,15 +1,31 @@
 /**
  * @param {string} s
- * @return {string}
+ * @return {string[]}
  */
-var replaceSpace = function (s) {
-  let res = "";
-  for (const c of s) {
-    if (c === " ") {
-      res += "%20";
-    } else {
-      res += c;
+var permutation = function (s) {
+  const res = [];
+  const str = s.split("");
+  dfs(0);
+  return res;
+
+  function dfs(x) {
+    if (x === str.length - 1) {
+      res.push(str.join(""));
+      return;
+    }
+    const set = new Set();
+    for (let i = x; i < str.length; i++) {
+      if (set.has(str[i])) {
+        continue;
+      }
+      set.add(str[i]);
+      swap(str, i, x);
+      dfs(x + 1);
+      swap(str, i, x);
     }
   }
-  return res;
+
+  function swap(str, i, j) {
+    [str[i], str[j]] = [str[j], str[i]];
+  }
 };
--- zi-fu-chuan-de-pai-lie-lcof.js
+++ zi-fu-chuan-de-pai-lie-lcof.js
@@ -32,4 +32,13 @@ var permutation = function (s) {
   }
 };
 
-console.log(permutation("abc"));
+/**
+ * 剑指 Offer 38. 字符串的排列
+ * 输入一个字符串，打印出该字符串中字符的所有排列。
+ * 你可以以任意顺序返回这个字符串数组，但里面不能有重复元素。
+ * 示例:
+ * 输入：s = "abc"
+ * 输出：["abc","acb","bac","bca","cab","cba"]
+ * 题解：
+ * https://leetcode-cn.com/problems/zi-fu-chuan-de-pai-lie-lcof/solution/mian-shi-ti-38-zi-fu-chuan-de-pai-lie-hui-su-fa-by/
+ */
